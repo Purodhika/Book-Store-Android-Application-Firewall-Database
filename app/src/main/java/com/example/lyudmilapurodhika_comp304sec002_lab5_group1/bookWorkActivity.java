d2d2fbeef1c6b2ce534c5805b61991193a7328b5
mila-zh <105249447+mila-zh@users.noreply.github.com>
2022-08-04 20:28:45 -0400

    mock data

--- app/src/main/java/com/example/lyudmilapurodhika_comp304sec002_lab5_group1/bookWorkActivity.java
+++ app/src/main/java/com/example/lyudmilapurodhika_comp304sec002_lab5_group1/bookWorkActivity.java
@@ -9,17 +9,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.List;
+
 public class bookWorkActivity extends AppCompatActivity {
     RecyclerView booksRV;
+    BooksViewModel viewModel;
+    BookAdapter booksAdapter;
+    List<Books> list;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_work);
 
+        //testing data
+        list.add(new Books("A","A", "A", 20));
+        list.add(new Books("B","B", "B", 10));
+        list.add(new Books("C","C", "C", 40));
+        list.add(new Books("D","D", "D", 24));
+        list.add(new Books("E","E", "E", 50));
         // prep recyclerview
         booksRV = findViewById(R.id.booksRV);
         booksRV.setLayoutManager(new LinearLayoutManager(this));
+
+        //fetch data
+        viewModel = new BooksViewModel(getApplication());
+
+        // pass selected songs into the recycler view
+        //rv.setLayoutManager(new LinearLayoutManager(this));
+       // adapter = new MyRVAdapter(this,  selectedSongs);
+       // rv.setAdapter(adapter);
 
     }
     //activity to display all books
